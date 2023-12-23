1e7a80e10789478e9db759a82d70dac153d399bd
DION <dionchristou@gmail.com>
2023-12-23 18:42:45 +0200

    Multiple Messages

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy and dion! second change");
+Console.WriteLine("Hello, C# Academy and dion! second change");
+Console.WriteLine("Hello, C# Academy and dion! second change");
+Console.WriteLine("Hello, C# Academy and dion! second change");
+Console.WriteLine("Hello, C# Academy and dion! second change");
